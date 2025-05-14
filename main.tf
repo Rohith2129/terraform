539017ed7949be83c3225d71b35e862543f914f4
Ubuntu <ubuntu@ip-172-31-36-36.ap-south-1.compute.internal>
2025-05-14 03:23:18 +0000

    Terraform for vpc and ec2

---
+++ main.tf
@@ -0,0 +1,73 @@
+provider "aws" {
+  region = "ap-south-1" 
+}
+
+resource "aws_vpc" "custom_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "custom-vpc"
+  }
+}
+
+resource "aws_subnet" "custom_subnet" {
+  vpc_id                  = aws_vpc.custom_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
+  tags = {
+    Name = "custom-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.custom_vpc.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.custom_subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.custom_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0e35ddab05955cf57" 
+  instance_type = "t2.medium"
+  subnet_id     = aws_subnet.custom_subnet.id
+  key_name      = "newpem"
+  security_groups = [aws_security_group.allow_ssh.name]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
